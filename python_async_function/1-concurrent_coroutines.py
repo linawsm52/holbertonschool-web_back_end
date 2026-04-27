f4d07fbef0b3f4309eba29722f45f77a43037c88
linawsm52 <lenaexo0@gmail.com>
2026-04-27 14:35:17 +0300

    new file

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for measuring execution time"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measures the total execution time for wait_n and returns total_time / n"""
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+
+    total_time = end_time - start_time
+    return total_time / n

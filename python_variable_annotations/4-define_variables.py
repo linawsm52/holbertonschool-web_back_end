3ee9a29d29d1399965e44d794c441f60629ffe98
linawsm52 <lenaexo0@gmail.com>
2026-04-27 14:31:31 +0300

    new file

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Module for a type-annotated sum_list function"""
-from typing import List
+"""Module that defines and annotates variables"""
 
 
-def sum_list(input_list: List[float]) -> float:
-    """Returns the sum of a list of floats"""
-    return sum(input_list)
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
